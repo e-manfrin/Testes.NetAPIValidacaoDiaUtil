32b7103557d297a06089514181ae13e0982dfe33
Emily keyt Manfrin <rm87198@fiap.com.br>
2022-05-06 13:07:03 -0300

    Testes unitários

---
+++ Testes.cs
@@ -0,0 +1,59 @@
+using DataAPI.Services;
+using System;
+using Xunit;
+
+namespace TestesUnitarios
+{
+    public class Testes
+    {
+        [Fact]
+        public void TesteFeriadoFixoNatal()
+        {
+            var feriadoFixo = FeriadosFixo.ConsultarFeriadoFixo(25, 12);
+            Assert.True(feriadoFixo.Feriado == true);
+            Assert.NotNull(feriadoFixo.Feriado);
+            Assert.NotEmpty(feriadoFixo.NomeFeriado);
+        }
+
+        [Fact]
+        public void TesteFeriadoMovelPascoa()
+        {
+            var feriadoMovel = FeriadoMovel.ConsultarFeriadoMovel(17, 04, 2022);
+            Assert.True(feriadoMovel.Feriado == true);
+            Assert.NotNull(feriadoMovel.Feriado);
+            Assert.NotEmpty(feriadoMovel.NomeFeriado);
+        }
+
+        [Fact]
+        public void TesteConversorDiasDaSemana()
+        {
+            DataService dataService = new DataService();
+            var validar = dataService.ConversorDiasDaSemana(new DateTime(2022, 5, 5));
+            Assert.True(validar.Equals("Quinta-feira"));
+        }
+
+        [Fact]
+        public void TesteValidarDataDiaUtil()
+        {
+            DataService dataService = new DataService();
+            var diaUtil = dataService.VerificarDiaUtil(new DateTime(2022, 5, 5));
+            Assert.True(diaUtil.DiaUtil == true);
+        }
+
+        [Fact]
+        public void TesteValidarDataFinalDeSemanaSabado()
+        {
+            DataService dataService = new DataService();
+            var diaUtil = dataService.VerificarDiaUtil(new DateTime(2022, 5, 7));
+            Assert.True(diaUtil.DiaUtil == false);
+        }
+
+        [Fact]
+        public void TesteValidarDataFinalDeSemanaDomingo()
+        {
+            DataService dataService = new DataService();
+            var diaUtil = dataService.VerificarDiaUtil(new DateTime(2022, 5, 8));
+            Assert.True(diaUtil.DiaUtil == false);
+        }
+    }
+}
